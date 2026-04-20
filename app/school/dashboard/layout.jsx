727b13f816e3b90fe73543266361d15b39c1ca82
Daniel Aboderin <aboderindaniel482@gmail.com>
2026-04-20 15:46:04 +0100

    feat: Remove PaymentGuard from dashboard layouts and disable payment link in sidebar

--- app/school/dashboard/layout.jsx
+++ app/school/dashboard/layout.jsx
@@ -3,8 +3,6 @@
 import { useState } from "react";
 import TopNav from "@/components/Navbar/TopNav";
 import Sidebar from "@/components/Navbar/Sidebar";
-import PaymentGuard from "@/services/PaymentGuard";
-
 export default function SchoolDashboardLayout({ children }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
@@ -14,9 +12,7 @@ export default function SchoolDashboardLayout({ children }) {
       <div className="bg-[#F5F6FA] w-full flex flex-col min-h-screen overflow-x-hidden md:ml-64">
         <TopNav onMenuClick={() => setSidebarOpen(true)} />
         <div className="flex-1 overflow-y-auto">
-          <PaymentGuard paymentRoute="/school/dashboard/payments">
-            {children}
-          </PaymentGuard>
+          {children}
         </div>
       </div>
     </main>
